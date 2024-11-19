c26bc1fb391e0cdbe5f71156f47104efa3e219d4
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-11-19 09:03:44 +0100

    add Not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,2 +1,17 @@
-const NotFound = () => {};
+import Nav from '../../components/Nav';
+import Footer from '../../components/Footer';
+
+const NotFound = () => {
+    return (
+    <>
+        <Nav />
+        <main class="main bg-dark notfound">
+            <div class="header">
+                <h1>404 Not Found</h1>
+            </div>
+        </main>
+        <Footer />
+    </>
+    );
+};
 export default NotFound;
