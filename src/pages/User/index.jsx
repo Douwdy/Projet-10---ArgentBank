aabdc8a428e7dc6e573647a8867aa3d15c56141b
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-11-26 09:18:02 +0100

    Integrate the username change form

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,10 +1,16 @@
 import Nav from '../../components/Nav';
 import Footer from '../../components/Footer';
 import Account from '../../components/BankAccount';
+import UserInfoForm from '../../components/UserInfoForm';
+
+const user = {
+    username: 'TonyJ',
+    firstName: 'Tony',
+    lastName: 'Jarvis'
+};
 
 // temporary hard-coded value
-const username = 'Tony Jarvis';
-
+const username = user.firstName + ' ' + user.lastName;
 
 const User = () => {
     return (
@@ -14,6 +20,7 @@ const User = () => {
       <div className="header">
         <h1>Welcome back<br />{username}!</h1>
         <button className="edit-button">Edit Name</button>
+        <UserInfoForm user={user}/>
       </div>
       <h2 className="sr-only">Accounts</h2>
         <h3 className="sr-only">Account</h3>
